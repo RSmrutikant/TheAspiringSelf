2eda84e898f761650b49549561f6fadcfd6a0efe
RSmrutikant <rsmrutikant8@gmail.com>
2023-01-29 00:32:50 +0530

    Added Video

--- assets/js/main.js
+++ assets/js/main.js
@@ -278,4 +278,45 @@
     });
   });
 
+//   window.addEventListener("scroll", function (event) {
+//     var scroll = this.scrollY;
+//     console.log(scroll)
+//     if(scroll >= 50){
+//       console.log("Triggered")
+//     document.getElementById('introVideo').play();
+//     }
+// });
+
+
+  (function(){
+    //gett all the video elements on the document
+    var vids = document.getElementsByClassName('autoplay_vid'); 
+    
+    //loop through all the video elements
+    for (var i = vids.length - 1; i >= 0; i--) {
+    	//pause all the videos by default
+    	vids[i].pause();
+    }
+    
+    //triger an anonymous function that handles the playy, pause video actions
+    window.onscroll = function(){
+        for (var i = vids.length - 1; i >= 0; i--) {
+            
+            //get current scrol position
+            var currentYpos = window.pageYOffset || document.documentElement.scrollTop;
+            
+    		if ( currentYpos >= vids[i].offsetTop 
+    		    && currentYpos <= vids[i].offsetTop + vids[i].offsetHeight 
+    		   ) {
+    		    //Play video if the current scroll position 
+    		    //is between the top and bottom of the video element
+    			vids[i].play();
+    		}else{
+    		    //else pause the video 
+    			vids[i].pause();
+    		}
+    	}
+    };
+});
+
 })()
